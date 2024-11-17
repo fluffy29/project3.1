5bee91cd306555d44b1a1c1b4ce6f88143c9f6e7
fluffy29 <hmajzoub12@gmail.com>
2024-11-17 22:25:27 +0100

    refactor: remove counter functionality and replace with card component

--- vite-project/components/cardComponent.js
+++ vite-project/components/cardComponent.js
@@ -1,8 +1,3 @@
-import { cardComponent } from "./components/cardComponent";
-import "./style.css";
-
-app.append(cardComponent());
-
 export const cardComponent = () => {
   const card = document.createElement("article");
   card.innerText = "hello from component card !";
