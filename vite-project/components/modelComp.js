38980b4796ae9b70967c15bb6026741958e3b547
fluffy29 <hmajzoub12@gmail.com>
2024-11-18 01:12:52 +0100

    feat: implement user actions component with edit and delete functionality

--- vite-project/components/modelComp.js
+++ vite-project/components/modelComp.js
@@ -1,11 +1,54 @@
-import "./modalComp.css";
+import "./userActions.css";
+import { actionButton } from "./actionButton ";
+import edit from "../assets/edit.svg";
+import trash from "../assets/trash.svg";
+import { formComp } from "./formComp";
+import { deleteUser } from "../utils/deleteUser";
+import { init } from "../main";
 
-export const modalComp = (titleText) => {
-  const modalOverLay = document.createElement("div");
+export const usersActions = () => {
+  const actionsContainer = document.createElement("div");
+  actionsContainer.classList.add("actions-container");
 
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") return modalOverLay.classList.remove("show");
-  });
+  const buttonOne = actionButton(edit, "warning-btn", buttonOneFn);
+  const buttonTwo = actionButton(trash, "danger-btn", buttonTwoFn);
 
-  return modalOverLay;
+  actionsContainer.appendChild(buttonOne);
+  actionsContainer.appendChild(buttonTwo);
+
+  return actionsContainer;
 };
+
+function buttonOneFn(e) {
+  const selectedCard = e.target.parentElement.parentElement;
+  const userId = selectedCard.getAttribute("userId");
+  const firstName = selectedCard.getAttribute("firstName");
+  const lastName = selectedCard.getAttribute("lastName");
+  document.querySelector(".modal-overlay").classList.toggle("show");
+  document.querySelector(".modal-container").innerHTML = "";
+  document.querySelector(".modal-title").innerText = "Edit user";
+  document
+    .querySelector(".modal-container")
+    .appendChild(formComp(firstName, lastName, userId));
+}
+
+function buttonTwoFn(e) {
+  const selectedCard = e.target.parentElement.parentElement;
+  const userId = selectedCard.getAttribute("userId");
+  document.querySelector(".modal-overlay").classList.toggle("show");
+  document.querySelector(".modal-container").innerHTML = "";
+  document.querySelector(".modal-title").innerText = "Warning !";
+  document.querySelector(".modal-container").innerText =
+    "Confirm delete user? You sure ?";
+
+  const confirmBtn = document.createElement("button");
+  confirmBtn.style.backgroundColor = "#d5384f";
+  confirmBtn.innerText = "Confirm";
+  confirmBtn.addEventListener("click", async () => {
+    const response = await deleteUser(userId);
+    if (response.message === "User deleted !") {
+      init();
+    }
+  });
+  document.querySelector(".modal-container").appendChild(confirmBtn);
+}
