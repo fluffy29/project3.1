932968653954d8d58ecd757f8867b343ea61ce2b
fluffy29 <hmajzoub12@gmail.com>
2024-11-18 01:11:36 +0100

    feat: implement form component with user input fields and submission handling

--- vite-project/components/inputComp.js
+++ vite-project/components/inputComp.js
@@ -8,7 +8,21 @@
  */
 
 export const inputComp = (type, id, innerText, value) => {
-	const inputContainer = document.createElement("div")
+  const inputContainer = document.createElement("div");
+  inputContainer.classList.add("input-container");
 
-	return inputContainer
-}
+  const formInput = document.createElement("input");
+  formInput.setAttribute("type", type);
+  formInput.classList.add("form-control");
+  formInput.setAttribute("id", id);
+  formInput.value = value;
+
+  const firstNameLabel = document.createElement("label");
+  firstNameLabel.setAttribute("for", id);
+  firstNameLabel.innerText = innerText;
+
+  inputContainer.appendChild(firstNameLabel);
+  inputContainer.appendChild(formInput);
+
+  return inputContainer;
+};
